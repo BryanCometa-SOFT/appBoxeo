38ad9402783b61657b240bbe27fe4b04cfcac056
Bryan Stiven Cometa Perdomo <bscometa@misena.edu.co>
2023-04-23 11:21:28 -0500

    ajustes del nombre

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'appboxeo',
+  appName: 'Boxea_Pro',
   webDir: 'www',
   bundledWebRuntime: false
 };
